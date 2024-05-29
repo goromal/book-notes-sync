5dd60f3c370ca1ee8d32567805a8c79cad14d3f8
Andrew Torgesen <atorgesen@anduril.com>
2024-05-29 10:11:52 -0700

    Consolidate google secrets

--- book_notes_sync/defaults.py
+++ book_notes_sync/defaults.py
@@ -1,11 +1,8 @@
 import os
 
 class BookNotesSyncDefaults:
-    DOCS_SECRETS_FILE = os.path.expanduser("~/secrets/docs/client_secrets.json")
-    DOCS_REFRESH_TOKEN = os.path.expanduser("~/secrets/docs/token.json")
-    DOCS_SCOPE = [
-        "https://www.googleapis.com/auth/documents.readonly"
-    ]
+    DOCS_SECRETS_FILE = os.path.expanduser("~/secrets/google/client_secrets.json")
+    DOCS_REFRESH_TOKEN = os.path.expanduser("~/secrets/google/refresh.json")
     WIKI_URL = "https://notes.andrewtorgesen.com"
     WIKI_SECRETS_FILE = os.path.expanduser("~/secrets/wiki/secrets.json")
     ENABLE_LOGGING = True
@@ -15,7 +12,6 @@ class BookNotesSyncDefaults:
         argname_mapping = {
             "docs_secrets_file": BookNotesSyncDefaults.DOCS_SECRETS_FILE,
             "docs_refresh_token": BookNotesSyncDefaults.DOCS_REFRESH_TOKEN,
-            "docs_scope": BookNotesSyncDefaults.DOCS_SCOPE,
             "wiki_url": BookNotesSyncDefaults.WIKI_URL,
             "wiki_secrets_file": BookNotesSyncDefaults.WIKI_SECRETS_FILE,
             "enable_logging": BookNotesSyncDefaults.ENABLE_LOGGING,
